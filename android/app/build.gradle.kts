45d51c1541fb4b3e6119033548599d701eb050f6
user1000 <lwn@gmail.com>
2025-11-27 12:04:57 +0100

    version: 1.0.3+15

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.qqchat.fast"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
